45f7ff5b956c08ba45ab24216ad0eeba14a4565a
Bruno Portos <brportos@c1r7s12.42antananarivo.mg>
2026-09-07 19:35:56 +0300

    codexion

---
+++ codexion/arg.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arg.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brportos <brportos@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/07 19:17:53 by brportos          #+#    #+#             */
+/*   Updated: 2026/09/07 19:27:40 by brportos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+static int	parse_data(t_data *data, int *argc);
+
+int	isargs_valid(t_data *data, char **argv)
+{
+	int	i;
+	int	parsed_argv[8];
+
+	i = 1;
+	while (i != 8)
+	{
+		parsed_argv[i - 1] = atoi(argv[i]);
+		if (parsed_argv[i - 1] >= 1)
+		{
+			if (i == 1 && parsed_argv[i - 1] >= MAX_CODERS)
+				return (display_error("coders must be < ", argv[i], data));
+			i++;
+		}
+		else
+			return (display_error("coders must be > ", argv[i], data));
+	}
+	if (parse_data(data, parsed_argv) == 1 || init_struct(data) == 1)
+		return (1);
+	if (strcmp(FIFO, argv[i]) == 0 || strcmp(EDT, argv[i]) == 0)
+		data->scheduler = argv[i];
+	else
+		return (display_error("Invalid argument ", argv[i], data));
+	return (0);
+}
+
+static int	parse_data(t_data *data, int *argc)
+{
+	data->coder = malloc(sizeof(t_coder) * argc[0]);
+	if (!data->coder)
+		return (display_error("Can't allocate ", NULL, data));
+	data->dongle = malloc(sizeof(t_dongle) * argc[0]);
+	if (!data->dongle)
+		return (display_error("Can't allocate ", NULL, data));
+	data->ncoder = argc[0];
+	data->ndongle = argc[0];
+	data->max_burnout = argc[1];
+	data->time_compile = argc[2];
+	data->time_debug = argc[3];
+	data->time_refactor = argc[4];
+	data->required_compile = argc[5];
+	data->dongle_cooldown = argc[6];
+	return (0);
+}
+
+int	add_to_queue(t_queue_manager *manager, t_coder *coder)
+{
+	t_queue	*queue;
+
+	queue = malloc(sizeof(t_queue));
+	if (!queue)
+		return (1);
+	queue->coder = coder;
+	queue->next = NULL;
+	if (manager->first == NULL)
+	{
+		manager->first = queue;
+		manager->last = queue;
+	}
+	else
+	{
+		manager->last->next = queue;
+		manager->last = queue;
+	}
+	return (0);
+}
---
+++ codexion/burnout.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   burnout.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brportos <brportos@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/07 19:17:58 by brportos          #+#    #+#             */
+/*   Updated: 2026/09/07 19:24:31 by brportos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+int	check_burnout(t_data *data, int *done)
+{
+	int	i;
+
+	i = 0;
+	*done = 0;
+	while (data->ncoder != i)
+	{
+		if (get_have_done(&data->coder[i]) == 1)
+			(*done)++;
+		else if ((get_time_ms()
+				- get_burnout(&data->coder[i])) > data->max_burnout)
+		{
+			stop_simulation(data);
+			display_log(data->coder[i].id, 0, "burns_out", data);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+long long	get_burnout(t_coder *coder)
+{
+	long long	burnout;
+
+	burnout = 0;
+	pthread_mutex_lock(&coder->mutex_burnout);
+	burnout = coder->time_burnout;
+	pthread_mutex_unlock(&coder->mutex_burnout);
+	return (burnout);
+}
+
+void	set_burnout(t_coder *coder)
+{
+	pthread_mutex_lock(&coder->mutex_burnout);
+	coder->time_burnout = get_time_ms();
+	pthread_mutex_unlock(&coder->mutex_burnout);
+}
